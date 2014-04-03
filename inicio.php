c06ae811933554948bc760d9d9f32061c4fe7bf0
JoseLuisLopez <luis11252009@hotmail.com>
2014-04-03 11:45:37 -0600

    Create inicio.php

---
+++ inicio.php
@@ -0,0 +1,16 @@
+<body> 
+Bienvenido root <p> <p>
+ <?php 
+ if (isset($_POST["root"])) 
+ { 
+ $root= $_POST['administrador']; 
+ $administrador= $_POST['administrador']; 
+ } 
+ 
+?>
+
+<script language="javaScript">
+ document.location.href = "practica7.html";
+ </script>
+ 
+ </body>
